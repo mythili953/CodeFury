00c78850ab48ff8a3aa8e553d78af7ec32428f0a
shr968 <shreyanayakb.261104@gmail.com>
2024-07-06 17:13:01 +0530

    resolved FAQs and added countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
-'use strict';
+// 'use strict';
 
+// for the faq section
+const faqs = document.querySelectorAll(".faq")
+faqs.forEach(faq=>{
+    faq.addEventListener("click",()=>{
+        faq.classList.toggle("active");
+    })
+})
+// js for countdown
+let daysItem = document.querySelector("#days")
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec")
 
+let countDown = ()=>{
+    let futureDate = new Date("31 July 2024");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+    let days = Math.floor(myDate/1000/60/60/24);
+    let hours = Math.floor(myDate/1000/60/60) % 24;
+    let min = Math.floor(myDate/1000/60)%60;
+    let sec = Math.floor(myDate/1000)%60;
+    daysItem.innerHTML=days;
+    hoursItem.innerHTML=hours;
+    minItem.innerHTML=min;
+    secItem.innerHTML=sec;
+}
+countDown();
+setInterval(countDown,1000);
 
 /**
  * add event on multiple elements
@@ -146,9 +173,3 @@ document.getElementById('read-more3').addEventListener('click', function() {
 });
 
 
-const faqs = document.querySelectorAll(".faq")
-faqs.forEach(faq=>{
-    faq.addEventListener("click",()=>{
-        faq.classList.toggle("active");
-    })
-})
